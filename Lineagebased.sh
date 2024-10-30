72d779fe98efdd7d9102036b02d85f015df95159
Sepidermn <zaidanagusta319@gmail.com>
2024-10-30 16:10:08 +0700

    Update Lineagebased.sh

--- Lineagebased.sh
+++ Lineagebased.sh
@@ -16,7 +16,7 @@ git clone https://github.com/lineageEXTmojito/kernel_xiaomi_mojito.git --depth 1
 git clone https://gitlab.com/Sepidermn/android_vendor_xiaomi_mojito.git --depth 1 -b 14 vendor/xiaomi/mojito
 git clone https://gitlab.com/Sepidermn/android_vendor_xiaomi_sm6150-common.git --depth 1 -b 14 vendor/xiaomi/sm6150-common
 # hardware tree
-git clone https://github.com/lineageEXTmojito/android_hardware_xiaomi.git --depth 1 -b mojito hardware/xiaomi
+git clone https://github.com/lineageEXTmojito/android_hardware_xiaomi.git --depth 1 -b universe hardware/xiaomi
 # set build environment
 . build/envsetup.sh
 # lunch
